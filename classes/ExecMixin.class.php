7e8057f9e0da427825a8486fb6ded66e3ac6b544
Ben Allfree <ben@benallfree.com>
2013-02-07 16:57:11 -0800

    Added support for exec() and cmd() methods

--- classes/ExecMixin.class.php
+++ classes/ExecMixin.class.php
@@ -1,7 +1,28 @@
 <?
+class Execer
+{
+  var $command;
+  var $output;
+  var $retval;
+  function __construct($cmd)
+  {
+    $this->command = $cmd;
+    $this->output = '';
+    $this->retval = null;
+  }
+  
+  function exec()
+  {
+    exec($this->command . " 2>&1", $out, $ret);
+    $this->output = $out;
+    $this->retval = $ret;
+    return $this;
+  }
+}
 
 class ExecMixin extends Mixin
 {
+
   static function interpolate()
   {
     $args = func_get_args();
@@ -27,30 +48,28 @@ class ExecMixin extends Mixin
     return $s;
   }
   
+  static function cmd_or_die($cmd, $expected_retval=0)
+  {
+    $args = func_get_args();
+    array_shift($args); // cmd
+    array_shift($args); // retval
+    array_unshift($args, $cmd); // put cmd back
+    $s = call_user_func_array('W::interpolate', $args);
+    $exec = new Execer($s);
+    $exec->exec();
+    if($exec->retval!=$expected_retval)
+    {
+      W::error("Execution failed.", $exec, $exec->retval);
+    }
+    return $exec;
+  }
+  
   static function cmd($cmd)
   {
     $args = func_get_args();
-    $s = call_user_func_array('interpolate', $args);
-    puts($s);
-    exec($s . " 2>&1",$output, $result);
-    if($result!=0)
-    {
-      puts("Error: $result");
-      puts($s);
-      puts($output);
-      W::error("Execution failed.");
-    }
-    return $output;
-  }
-
-  static function exec($cmd, $expected_retval=0, &$out='')
-  {
-    exec($cmd . " 2>&1", $out, $ret);
-    if ($ret != $expected_retval)
-    {
-      W::error("Command failed: $cmd", array($ret, $out));
-    }
-    return $ret;
-    
+    $s = call_user_func_array('W::interpolate', $args);
+    $exec = new Execer($s);
+    $exec->exec();
+    return $exec;
   }
 }
